31e44dc20998b2cf4791c6397ba602fb1b7ad72b
Ubuntu <ubuntu@ip-172-31-28-160.ap-southeast-1.compute.internal>
2015-12-12 13:04:33 +0000

    nightly build 1

--- define/settings.py
+++ define/settings.py
@@ -122,13 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/", "static_root")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
