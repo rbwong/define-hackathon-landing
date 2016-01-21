23210c9dd19da8620f595364fa833ad0b5631d52
Ryan Wong <ryan.wong022@gmail.com>
2016-01-21 21:34:40 +0800

    fixed css

--- define/settings.py
+++ define/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^@k9mj7qwv&g2lwf%q_kp!u55^(+%3mdjnlg40m&2l$ie8ai+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*', ]
 
 
